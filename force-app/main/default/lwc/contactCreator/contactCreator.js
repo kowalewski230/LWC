e6d0a9fca85b36d00ea361265008d77d4653c478
krzysztof.kowalewski.ext <zhP!Vda5.j4w96>
2021-03-02 19:06:37 +0100

    Use Lightning Data Service to Work with Data

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -6,7 +6,7 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LASTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 export default class ContactCreator extends LightningElement {
     objectApiName = CONTACT_OBJECT;
