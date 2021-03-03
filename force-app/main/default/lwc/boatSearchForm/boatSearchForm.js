a8ecba151aad6e36335a1575c0fd874968b838c4
krzysztof.kowalewski.ext <zhP!Vda5.j4w96>
2021-03-04 00:56:38 +0100

    Build the Boat Message Service channel Get BoatDataService class ready for action Build the component boatSearchForm

---
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -0,0 +1,37 @@
+// imports
+import getBoatTypes from "@salesforce/apex/BoatDataService.getBoatTypes";
+import { LightningElement, wire, track } from "lwc";
+export default class BoatSearchForm extends LightningElement {
+  selectedBoatTypeId = "";
+
+  // Private
+  error = undefined;
+
+  @track searchOptions;
+
+  @wire(getBoatTypes)
+  boatTypes({ error, data }) {
+    if (data) {
+      this.searchOptions = data.map((type) => {
+        return { label: type.Name, value: type.Id };
+      });
+      this.searchOptions.unshift({ label: "All Types", value: "" });
+      console.log(this.searchOptions);
+    } else if (error) {
+      this.searchOptions = undefined;
+      this.error = error;
+    }
+  }
+
+  // Fires event that the search option has changed.
+  // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
+  handleSearchOptionChange(event) {
+    // Create the const searchEvent
+    // searchEvent must be the new custom event search
+    this.selectedBoatTypeId = event.detail.value;
+    const searchEvent = new CustomEvent("search", {
+      detail: { boatTypeId: this.selectedBoatTypeId }
+    });
+    this.dispatchEvent(searchEvent);
+  }
+}
